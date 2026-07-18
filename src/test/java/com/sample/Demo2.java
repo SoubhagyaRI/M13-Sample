cd6da8c4b9cc54122ca914f16caead05bf365700
SoubhagyaRI <soumyingalagi18@gmail.com>
2026-07-18 10:07:02 +0530

    Git conflict practice from ATE

--- src/test/java/com/sample/Demo2.java
+++ src/test/java/com/sample/Demo2.java
@@ -5,7 +5,7 @@ public class Demo2 {
 	public static void main(String[] args) {
 		System.out.println("Github practice");
 		System.out.println("In ATE branch");
-		System.out.println("practice Git practice from ATE system ");
+		System.out.println("practice Git practice from ATE system");
 	}
 
 }
